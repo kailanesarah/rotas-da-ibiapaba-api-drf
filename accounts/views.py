b8c5c396a27c3cd720bda88f30fe62cf2248e3fa
Kailane Sarah <kailanesarahpro@gmail.com>
2025-06-03 14:45:28 -0300

    feat(accounts): integrate custom cookie-based authentication and fix serializer output
    - Updated accounts view to use custom JWT authentication via cookies.
    - Fixed syntax in serializers.py to return complete category object with id and name.

--- accounts/views.py
+++ accounts/views.py
@@ -2,12 +2,14 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.generics import ListCreateAPIView
 from accounts.models import Establishment
 from accounts.serializers import EstablishementSerializer
+from authentication.authentication import CookieJWTAuthentication
 
 
 class EstablishmentListCreateView(ListCreateAPIView):
     permission_classes = (IsAuthenticated, )
     queryset = Establishment.objects.all()
     serializer_class = EstablishementSerializer
+    authentication_classes = [CookieJWTAuthentication] 
 
     def get_permissions(self):
         if self.request.method == 'POST':
