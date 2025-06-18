dd646632f544070b9a13ea9752399109b48218e9
Nextech <nextechbusiness24@gmail.com>
2025-06-18 14:36:48 -0300

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from accounts.views import EstablishmentListCreateView, AdminListCreateView
 
 urlpatterns = [
-    path('establishment/', EstablishmentListCreateView.as_view(), name="establishment_Create_List_View"),
-    path('admin/', AdminListCreateView.as_view(), name='create_admin')
+    path('establishment/', EstablishmentListCreateView.as_view(), name="establishment_create_list_view"),
+    path('admin/', AdminListCreateView.as_view(), name='admin_create_list_view')
 ]
 
