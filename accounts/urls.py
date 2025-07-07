0baee64006558afaadfe636430961f11cac8b074
Kailane Sarah <kailanesarahpro@gmail.com>
2025-07-07 11:34:55 -0300

    fix: ajusta urls para incluir a url de detail view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from accounts.views import EstablishmentListCreateView, AdminListCreateView
+from accounts.views import EstablishmentListCreateView, AdminListCreateView, DetailEstablishment
 from django.conf.urls.static import static
 from django.conf import settings
 
+
 urlpatterns = [
-    path('establishment/', EstablishmentListCreateView.as_view(),name="establishment_create_list_view"),
-    path('admin/', AdminListCreateView.as_view(), name='admin_create_list_view')
-    
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('establishment/', EstablishmentListCreateView.as_view(), name="establishment_create_list_view"),
+    path('establishment/<int:pk>/', DetailEstablishment.as_view(), name="establishment_list_detail"),
+    path('admin/', AdminListCreateView.as_view(), name='admin_create_list_view'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
