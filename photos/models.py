2dd44099478a7d2a7b9c9ee0a23581a5619c83ba
Kailane Sarah <75998609+kailanesarah@users.noreply.github.com>
2025-07-08 17:24:29 -0300

    refactor(photos): destaca campos booleanos no model e cria views para upload
    - Refatorado o model Photo, destacando os campos booleanos (is_profile_pic, is_gallery_pic, is_product_pic).
    - Atualizado o serializer para refletir os novos campos.
    - Criadas duas views:
      - Upload de imagem de perfil do estabelecimento.
      - Upload de múltiplas imagens para a galeria.

--- photos/models.py
+++ photos/models.py
@@ -1,14 +1,37 @@
 from django.db import models
+from accounts.models import Establishment
 
 
-class EstablishmentProfileImage(models.Model):
-    image = models.ImageField(
-        upload_to='profile_photos/', null=True, blank=True)
+#retorna um caminho tipo: photos/{id_do_estabelecimento}/profile/{nome_do_arquivo} o mesmo para os demais
+def upload_to_path(instance, filename):
+    if instance.is_profile_pic:
+        return f'photos/{instance.establishment.id}/profile/{filename}'
+    elif instance.is_gallery_pic:
+        return f'photos/{instance.establishment.id}/gallery/{filename}'
+    elif instance.is_product_pic:
+        return f'photos/{instance.establishment.id}/product/{filename}'
+    return f'photos/{instance.establishment.id}/others/{filename}'
+
+
+class Photo(models.Model):
+    establishment = models.ForeignKey(
+        Establishment,
+        on_delete=models.CASCADE,
+        related_name='photos'  # Permite: estabelecimento.photos.all()
+    )
+    image = models.ImageField(upload_to=upload_to_path)
+    alt_text = models.CharField(max_length=255, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    alt_text = models.CharField(
-        max_length=255,
-        blank=True,
-        null=True,
-        help_text="Texto alternativo para acessibilidade"
-    )
+
+    is_profile_pic = models.BooleanField(default=False)
+    is_gallery_pic = models.BooleanField(default=False)
+    is_product_pic = models.BooleanField(default=False)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['establishment', 'is_profile_pic']),
+            models.Index(fields=['establishment', 'is_gallery_pic']),
+            models.Index(fields=['establishment', 'is_product_pic']),
+        ]
+
